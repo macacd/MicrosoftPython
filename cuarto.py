55f1a4fc66e7a5136c4e05cb1d3d77be184c364c
macacd <57044387+macacd@users.noreply.github.com>
2020-11-15 20:01:49 +0100

    n

--- cuarto.py
+++ cuarto.py
@@ -2,11 +2,11 @@
 
 canada = input('Eres de canada (Y/N)')
 
-if canada.lower == 'y' :
+if canada.lower() == 'y' :
     provincia = input ('de que provincia?')
-    if provincia.lower in('alberta', 'nunavut','yukon'):
+    if provincia.lower() in('alberta', 'nunavut','yukon'):
         tax = 0.5
-    elif provincia.lower == 'ontario' :
+    elif provincia.lower() == 'ontario' :
         tax = 0
 else :
     tax = 0.15
