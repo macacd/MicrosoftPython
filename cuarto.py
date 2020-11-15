fd64943240bb3fe14ce588d811708fb29baa156f
macacd <57044387+macacd@users.noreply.github.com>
2020-11-15 19:45:28 +0100

    nuevo

---
+++ cuarto.py
@@ -0,0 +1,16 @@
+#Estrcuturas condicionales
+
+canada = input('Eres de canada (Y/N)')
+
+if canada.lower == 'y' :
+    provincia = input ('de que provincia?')
+    if provincia.lower in('alberta', 'nunavut','yukon'):
+        tax = 0.5
+    elif provincia.lower == 'ontario' :
+        tax = 0
+else :
+    tax = 0.15
+
+print (f'La tasa de impuestos es {str(tax)}')
+
+
